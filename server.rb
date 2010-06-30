057e04224023d9dd6a4802c87df581aa57679c53
lizconlan <liz@work.machine>
2010-06-30 11:00:55 +0100

    starting point for server.rb file

---
+++ server.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'haml'
+require 'sass'
+require 'mongo'
+
+enable :sessions
+
+def self.get_mongo_connection
+  if ENV['RACK_ENV'] && ENV['RACK_ENV'] == 'production'
+    db_name = ENV['MONGO_DB']
+    db_server = ENV['MONGO_SERVER']
+    db_port = ENV['MONGO_PORT']
+    db_user = ENV['MONGO_USER']
+    db_pass = ENV['MONGO_PASS']
+  else    
+    mongo_conf = YAML.load(File.read('config/virtualserver/mongo.yml'))
+    db_name = mongo_conf[:db]
+    db_server = mongo_conf[:server]
+    db_port = mongo_conf[:port]
+    db_user = mongo_conf[:user]
+    db_pass = mongo_conf[:pass]
+  end
+
+  db = Mongo::Connection.new(db_server, db_port).db(db_name)
+  db.authenticate(db_user, db_pass)
+  
+  return db
+end
+
+MONGO_DB = get_mongo_connection()
