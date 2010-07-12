17801be6fed78bb9d859dfaa7950f3915b1120fa
lizconlan <liz@work.machine>
2010-07-12 14:40:57 +0100

    removed the about stuff

--- server.rb
+++ server.rb
@@ -33,43 +33,6 @@ end
 MONGO_DB = get_mongo_connection()
 
 get '/' do
-  do_auth()
-  
-  unless session[:seen]
-    session[:seen] = ""
-  end
-  
-  unwanted = session[:seen]
-  
-  coll = MONGO_DB.collection("flags")
-  
-  if unwanted == ""
-    photos = coll.find()
-  else
-    photos = coll.find({"photo_id" => { "$not" => /#{unwanted}/ } })
-  end
-  
-  if photos.count > 0
-  @count = photos.count  
-    @first = photos.next_document
-  
-    if session[:seen] == ""
-      session[:seen] = "^#{@first['photo_id']}$"
-    else
-      session[:seen] += "|^#{@first['photo_id']}$"
-    end
-  
-    @mp_name = format_name_for_url(@first['name'])
-  
-    haml :index
-  else
-    haml :no_photo
-  end
-end
-
-get "/about/?" do
-  haml :about
-end
 
 get "/unflag/:photo_id/?" do
   do_auth()
